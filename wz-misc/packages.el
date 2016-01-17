b7ad3387140ba2affef0f30fe1487cbf251022da
Walmes Zeviani <walmes@ufpr.br>
2016-01-17 13:21:38 -0200

    Add yafolgin to wz-misc layer.

--- wz-misc/packages.el
+++ wz-misc/packages.el
@@ -1,6 +1,7 @@
 (setq wz-misc-packages
   '(
     bm
+    yafolding
     ))
 
 (defun wz-misc/init-bm ()
@@ -14,6 +15,16 @@
   (with-eval-after-load 'bm
     (global-set-key (kbd "<C-f2>") 'bm-toggle)
     (global-set-key (kbd "<f2>") 'bm-next)
-    (global-set-key (kbd "<S-f2>") 'bm-previous)
-    )
+    (global-set-key (kbd "<S-f2>") 'bm-previous))
   ) ;; wz-misc/init-bm
+
+(defun wz-misc/init-yafolding ()
+  (use-package yafolding
+    :init
+    (progn
+      (yafolding-mode))
+    :config
+    (progn
+      (global-set-key [?\C-{] #'yafolding-hide-parent-element)
+      (global-set-key [?\C-}] #'yafolding-toggle-element)))
+  ) ;; wz-misc/init-yafolding
