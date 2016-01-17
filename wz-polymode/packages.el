7de1e4943aa0466e66024ca2d631a4f6f636f476
Walmes Zeviani <walmes@ufpr.br>
2016-01-17 14:21:39 -0200

    Add wz-polymode layer.

---
+++ wz-polymode/packages.el
@@ -0,0 +1,24 @@
+(setq wz-polymode-packages
+  '(
+    polymode
+    ))
+
+(defun wz-misc/init-polymode ()
+  (use-package polymode
+    :mode (("\\.Rmd"   . rmd-mode)
+           ("\\.Rpres" . rmd-mode))
+    :init
+    (progn
+      (defun rmd-mode ()
+        "ESS Markdown mode for rmd files"
+        (interactive)
+        (require 'poly-R)
+        (require 'poly-markdown)
+        (R-mode)
+        (poly-markdown+r-mode))
+      ))
+  (with-eval-after-load 'rmd-mode
+    (global-set-key (kbd "S-<f7>") 'polymode-previous-chunk)
+    (global-set-key (kbd "S-<f8>") 'polymode-next-chunk)
+    (global-set-key (kbd "S-<f9>") 'polymode-insert-new-chunk))
+  ) ;; wz-misc/init-polymode
