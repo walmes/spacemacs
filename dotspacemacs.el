ddcb6a2ba3a3a852b4cef2a047bb1c3a0da36da6
Walmes Zeviani <walmes@ufpr.br>
2016-01-17 14:12:47 -0200

    Add markdown layer.

--- dotspacemacs.el
+++ dotspacemacs.el
@@ -23,11 +23,11 @@ values."
      ;; Uncomment some layer names and press <SPC f e R> (Vim style) or
      ;; <M-m f e R> (Emacs style) to install them.
      ;; ----------------------------------------------------------------
-     ;; auto-completion
+     auto-completion
      ;; better-defaults
      emacs-lisp
      ;; git
-     ;; markdown
+     markdown
      ;; org
      ;; (shell :variables
      ;;        shell-default-height 30
