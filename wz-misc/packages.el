1f63d8a7d21979672601c168ac532308a8e20fc5
Walmes Zeviani <walmes@ufpr.br>
2016-01-17 13:26:55 -0200

    Add bookmark+ to wz-misc layer.

--- wz-misc/packages.el
+++ wz-misc/packages.el
@@ -2,6 +2,7 @@
   '(
     bm
     yafolding
+    bookmark+
     ))
 
 (defun wz-misc/init-bm ()
@@ -28,3 +29,26 @@
       (global-set-key [?\C-{] #'yafolding-hide-parent-element)
       (global-set-key [?\C-}] #'yafolding-toggle-element)))
   ) ;; wz-misc/init-yafolding
+
+(defun wz-misc/init-bookmark+ ()
+  (use-package bookmark+
+    :init
+    (progn
+      (setq bookmark-default-file "~/Dropbox/bookmarks"
+            bookmark-save-flag 1))
+    :config
+    (progn
+      ;; Create an autonamed bookmark.
+      (global-set-key
+       (kbd "<C-f3>") 'bmkp-toggle-autonamed-bookmark-set/delete)
+      ;; Go to the next bookmark in file.
+      (global-set-key
+       (kbd "<f3>") 'bmkp-next-bookmark-this-file/buffer-repeat)
+      ;; Go to the previous bookmark in file.
+      (global-set-key
+       (kbd "<f4>") 'bmkp-previous-bookmark-this-file/buffer-repeat)
+      ;; Toggle temporary/permanent bookmark.
+      (global-set-key
+       (kbd "<S-f3>") 'bmkp-toggle-temporary-bookmark))
+    )
+  ) ;; wz-misc/init-bookmark+
