9f015dfc5ce3da0d7a71c9c620d8098e19501209
Walmes Zeviani <walmes@ufpr.br>
2023-10-05 09:52:29 -0400

    Copy configurations from Mayer.

--- wz-ess/packages.el
+++ wz-ess/packages.el
@@ -9,26 +9,40 @@
 ;;
 ;;; License: GPLv3
 
-(setq wz-ess-packages
+(setq ess-packages
   '(
-    company
+    ;; company
     ess
     ess-R-data-view
-    ess-R-object-popup
+    ;; ess-R-object-popup
     ess-smart-equals
+    rainbow-delimiters
+    (electric-spacing-r :location local)
+    (essh :location local)
+    smartparens
+    ;; :local electric-spacing-r
     ))
 
-(defun wz-ess/init-ess ()
+(defun ess/init-ess ()
   (use-package ess-site
     :mode (("\\.sp\\'"           . S-mode)
            ("/R/.*\\.q\\'"       . R-mode)
            ("\\.[qsS]\\'"        . S-mode)
+           ("\\.ssc\\'"          . S-mode)
+           ("\\.SSC\\'"          . S-mode)
            ("\\.[rR]\\'"         . R-mode)
            ("\\.[rR]nw\\'"       . Rnw-mode)
            ("\\.[sS]nw\\'"       . Snw-mode)
            ("\\.[rR]profile\\'"  . R-mode)
            ("NAMESPACE\\'"       . R-mode)
            ("CITATION\\'"        . R-mode)
+           ("\\.omg\\'"          . omegahat-mode)
+           ("\\.hat\\'"          . omegahat-mode)
+           ("\\.lsp\\'"          . XLS-mode)
+           ("\\.do\\'"           . STA-mode)
+           ("\\.ado\\'"          . STA-mode)
+           ("\\.[Ss][Aa][Ss]\\'" . SAS-mode)
+           ("\\.jl\\'"           . ess-julia-mode)
            ("\\.[Ss]t\\'"        . S-transcript-mode)
            ("\\.Sout"            . S-transcript-mode)
            ("\\.[Rr]out"         . R-transcript-mode)
@@ -39,74 +53,93 @@
            ("\\.[Jj][Aa][Gg]\\'" . ess-jags-mode)
            ("\\.[Jj][Oo][Gg]\\'" . ess-jags-mode)
            ("\\.[Jj][Mm][Dd]\\'" . ess-jags-mode))
-    :commands (R)
+    :commands (R stata julia SAS)
     :init
     (progn
       (when (configuration-layer/package-usedp 'company)
-        (add-hook 'ess-mode-hook 'company-mode))
-      (add-hook 'ess-mode-hook
-                '(lambda ()
-                   (setq ess-indent-with-fancy-comments nil)
-                   (setq-local comment-add 0)
-                   ;; (ess-R-smart-operators t)
-                   (setq ess-smart-operators t)
-                   ))
+        (add-hook 'ess-mode-hook 'company-mode)))
+    (use-package ess-r-mode
+      :bind
+      (:map ess-r-mode-map
+            ("M--" . ess-insert-assign)
+            ("M-p" . add-pipe-magrittr)
+            ("M-o" . add-pipe-native)
+            ("M-k" . add-knitr-opts))
+      (:map inferior-ess-r-mode-map
+            ("M--" . ess-insert-assign)
+            ("M-p" . add-pipe-magrittr-inf)
+            ("M-o" . add-pipe-native-inf))
+      :config
+      (defun add-pipe-magrittr ()
+        (interactive)
+        (end-of-line)
+        (unless (looking-back "%>%" nil)
+          (just-one-space 1)
+          (insert "%>%"))
+        (newline-and-indent))
+      (defun add-pipe-magrittr-inf ()
+        (interactive)
+        (end-of-line)
+        (unless (looking-back "%>%" nil)
+          (just-one-space 1)
+          (insert "%>% ")))
+      (defun add-pipe-native ()
+        (interactive)
+        (end-of-line)
+        (unless (looking-back "|>" nil)
+          (just-one-space 1)
+          (insert "|>"))
+        (newline-and-indent))
+      (defun add-pipe-native-inf ()
+        (interactive)
+        (end-of-line)
+        (unless (looking-back "|>" nil)
+          (just-one-space 1)
+          (insert "|> ")))
+      (defun add-knitr-opts ()
+        (interactive)
+        (beginning-of-line)
+        (unless (looking-back "#|" nil)
+          (just-one-space 0)
+          (insert "#| ")
+          (end-of-line)))
       ))
 
-  ;; R -----------------------------------------------------------------
+  ;; R --------------------------------------------------------------------------
   (with-eval-after-load 'ess-site
-    ;; Follow Hadley Wickham's R style guide
-    (setq ess-first-continued-statement-offset 4
-          ess-continued-statement-offset 0
-          ess-expression-offset 4
-          ess-nuke-trailing-whitespace-p t
-          ess-default-style 'C++)
-    (setq-default inferior-R-args
-                  "--no-restore-history --no-save ")
-    (setq comint-scroll-to-bottom-on-input t)
-    (setq comint-scroll-to-bottom-on-output t)
-    (setq comint-move-point-for-output t)
-    (setq ess-R-font-lock-keywords
-          '((ess-R-fl-keyword:modifiers . t)
-            (ess-R-fl-keyword:fun-defs . t)
-            (ess-R-fl-keyword:keywords . t)
-            (ess-R-fl-keyword:assign-ops . t)
-            (ess-R-fl-keyword:constants . t)
-            (ess-fl-keyword:fun-calls . t)
-            (ess-fl-keyword:numbers . t)
-            (ess-fl-keyword:operators . t)
-            (ess-fl-keyword:delimiters . t)
-            (ess-fl-keyword:= . t)
-            (ess-R-fl-keyword:F&T . t)))
-    (setq inferior-R-font-lock-keywords
-          '((ess-S-fl-keyword:prompt . t)
-            (ess-R-fl-keyword:messages . t)
-            (ess-R-fl-keyword:modifiers . t)
-            (ess-R-fl-keyword:fun-defs . t)
-            (ess-R-fl-keyword:keywords . t)
-            (ess-R-fl-keyword:assign-ops . t)
-            (ess-R-fl-keyword:constants . t)
-            (ess-fl-keyword:matrix-labels . t)
-            (ess-fl-keyword:fun-calls . t)
-            (ess-fl-keyword:numbers . t)
-            (ess-fl-keyword:operators . t)
-            (ess-fl-keyword:delimiters . t)
-            (ess-fl-keyword:= . t)
-            (ess-R-fl-keyword:F&T . t)))
-    (defadvice ess-eval-buffer (before really-eval-buffer compile activate)
-      "Prevent call ess-eval-buffer by accident, frequently by
-       hitting C-c C-b instead of C-c C-n."
-      (if (yes-or-no-p
-           (format "Are you sure you want to evaluate the %s buffer?"
-                   buffer-file-name))
-          (message "ess-eval-buffer started.")
-        (error "ess-eval-buffer canceled!")))
+    ;; ESS
+    (add-hook 'ess-mode-hook
+              (lambda ()
+                (ess-set-style 'C++ 'quiet)
+                ;; Because
+                ;;                                 DEF GNU BSD K&R C++
+                ;; ess-indent-level                  2   2   8   5   4
+                ;; ess-continued-statement-offset    2   2   8   5   4
+                ;; ess-brace-offset                  0   0  -8  -5  -4
+                ;; ess-arg-function-offset           2   4   0   0   0
+                ;; ess-expression-offset             4   2   8   5   4
+                ;; ess-else-offset                   0   0   0   0   0
+                ;; ess-close-brace-offset            0   0   0   0   0
+                (add-hook 'local-write-file-hooks
+                          (lambda ()
+                            (ess-nuke-trailing-whitespace)))))
+    (setq ess-nuke-trailing-whitespace-p 'ask)
+    ;; or even
+    ;; (setq ess-nuke-trailing-whitespace-p t)
+    ;; Perl
+    (add-hook 'perl-mode-hook
+              (lambda () (setq perl-indent-level 4)))
 
     (defun spacemacs/ess-start-repl ()
       "Start a REPL corresponding to the ess-language of the current buffer."
       (interactive)
-      (cond ((string= "S" ess-language) (call-interactively 'R))))
+      (cond
+       ((string= "S" ess-language) (call-interactively 'R))
+       ((string= "STA" ess-language) (call-interactively 'stata))
+       ((string= "SAS" ess-language) (call-interactively 'SAS))))
 
+    (spacemacs/set-leader-keys-for-major-mode 'ess-julia-mode
+      "si" 'julia)
     (spacemacs/set-leader-keys-for-major-mode 'ess-mode
       "si" 'spacemacs/ess-start-repl
       ;; noweb
@@ -134,30 +167,104 @@
       )
     (define-key ess-mode-map (kbd "<s-return>") 'ess-eval-line)
     (define-key inferior-ess-mode-map (kbd "C-j") 'comint-next-input)
-    (define-key inferior-ess-mode-map (kbd "C-k") 'comint-previous-input)
-    ;; Movement across chunks in Rnw files.
-    (global-set-key (kbd "C-S-<f5>") 'ess-eval-chunk)
-    (global-set-key (kbd "C-S-<f6>") 'ess-eval-chunk-and-step)
-    (global-set-key (kbd "C-S-<f7>") 'ess-noweb-next-code-chunk)
-    (global-set-key (kbd "C-S-<f8>") 'ess-noweb-previous-code-chunk)
-    (global-set-key (kbd "C-S-<f9>") 'ess-noweb-goto-chunk)
-    ;; Easy navigation in s-exp.
-    (global-set-key (kbd "<M-S-up>") 'backward-up-list)
-    (global-set-key (kbd "<M-S-down>") 'down-list)
-    (global-set-key (kbd "<M-right>") 'forward-sexp)
-    (global-set-key (kbd "<M-left>") 'bakward-sexp)
-    (global-set-key (kbd "<M-up>") 'backward-list)
-    (global-set-key (kbd "<M-down>") 'forward-list)))
+    (define-key inferior-ess-mode-map (kbd "C-k") 'comint-previous-input)))
 
-(defun wz-ess/init-ess-R-data-view ())
+(defun ess/init-ess-R-data-view ())
 
-(defun wz-ess/init-ess-R-object-popup ())
+(defun ess/init-ess-R-object-popup ())
 
-(defun wz-ess/init-ess-smart-equals ()
+(defun ess/post-init-rainbow-delimiters ()
+  (add-hook 'ess-mode-hook #'rainbow-delimiters-mode))
+
+;; To enable smart-equals-mode
+(defun ess/init-ess-smart-equals ()
   (use-package ess-smart-equals
     :defer t
     :if ess-enable-smart-equals
     :init
+    ;; (setq ess-smart-equals-extra-ops '(brace paren percent))
     (progn
       (add-hook 'ess-mode-hook 'ess-smart-equals-mode)
       (add-hook 'inferior-ess-mode-hook 'ess-smart-equals-mode))))
+
+;; To enable smartparens-mode in ess and iess
+(defun ess/post-init-smartparens ()
+  (use-package smartparens
+    :defer t
+    :if ess-enable-smartparens
+    :diminish smartparens-mode
+    :config
+    (progn
+      (require 'smartparens-config)
+      (smartparens-global-mode 1)
+      (sp-pair "\"" nil :unless '(sp-point-after-word-p))
+      (sp-pair "'" nil :unless '(sp-point-after-word-p))
+      (sp-local-pair '(ess-mode inferior-ess-mode) "(" nil :unless '(sp-point-before-word-p))
+      (sp-local-pair '(ess-mode inferior-ess-mode) "[" nil :unless '(sp-point-before-word-p))
+      (sp-local-pair '(ess-mode inferior-ess-mode) "{" nil :unless '(sp-point-before-word-p)))
+    :init
+    (progn
+      (add-hook 'ess-mode-hook 'smartparens-mode)
+      (add-hook 'inferior-ess-mode-hook 'smartparens-mode))))
+
+;; To enable electric-spacing-mode in ess and iess
+(defun ess/init-electric-spacing-r ()
+  (use-package electric-spacing-r
+    ;; :defer t
+    :if ess-enable-electric-spacing-r
+    :init
+    (progn
+      (add-hook 'ess-mode-hook 'electric-spacing-mode))))
+      ;; (add-hook 'inferior-ess-mode-hook 'electric-spacing-mode))))
+
+(defun ess/init-essh ()
+  (use-package essh
+    :config
+    (progn
+      (add-hook
+       'sh-mode-hook
+       #'(lambda ()
+          (define-key sh-mode-map "\C-c\C-r" 'pipe-region-to-shell)
+          (define-key sh-mode-map "\C-c\C-b" 'pipe-buffer-to-shell)
+          (define-key sh-mode-map "\C-c\C-j" 'pipe-line-to-shell)
+          ;; (define-key sh-mode-map "\C-c\C-n" 'pipe-line-to-shell-and-step)
+          (define-key sh-mode-map (kbd "<C-return>") 'pipe-line-to-shell-and-step)
+          (define-key sh-mode-map "\C-c\C-f" 'pipe-function-to-shell)
+          (define-key sh-mode-map "\C-c\C-d" 'shell-cd-current-directory))))))
+
+(defun wz-ess-find-and-insert-namespace (beg end)
+  "Preceds a function with its namespace,
+   so `mean(x) -> stats::mean(x)' and
+   `xyplot(...) -> lattice::xyplot()'.
+   Call this function in a R major mode buffer with the function name
+   selected. By Walmes Zeviani."
+  (interactive "r")
+  (let ((string
+         (replace-regexp-in-string
+          "\"" "\\\\\\&"
+          (replace-regexp-in-string
+           "\\\\\"" "\\\\\\&"
+           (buffer-substring-no-properties beg end))))
+        (buf (get-buffer-create "*ess-command-output*")))
+    (ess-force-buffer-current "Process to load into:")
+    (ess-command
+     (format
+      "local({
+           x <- \"%s\"
+           cat(paste0(sub('.*:', '', utils::find(x)), '::', x), \"\\n\")
+       })\n"
+      string) buf)
+    (with-current-buffer buf
+      (goto-char (point-max))
+      (let ((end (point)))
+        (goto-char (point-min))
+        (skip-chars-forward " +")
+        (setq string (buffer-substring-no-properties (point) end))))
+    (delete-region beg end)
+    (insert string)
+    (delete-char -1)))
+
+(add-hook
+ 'ess-mode-hook
+ (lambda ()
+   (local-set-key (kbd "C-:") 'wz-ess-find-and-insert-namespace)))
