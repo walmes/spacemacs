a7ba80c3e8f645f62aabf97736f9ef6482868ca7
Walmes Zeviani <walmes@ufpr.br>
2023-10-05 16:54:35 -0400

    Adds polymode layer based on Mayer's one.

---
+++ polymode/packages.el
@@ -0,0 +1,43 @@
+;;; packages.el --- polymode layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: Walmes Zeviani & Fernando Mayer
+;; URL: https://github.com/syl20bnr/spacemacs
+
+;;; Code:
+
+(defconst polymode-packages
+  '(polymode))
+
+(defun polymode/init-polymode ()
+  (use-package polymode
+    :mode (("\\.Rmd" . Rmd-mode))
+    :init
+    (progn
+      (defun Rmd-mode ()
+        "ESS Markdown mode for Rmd files"
+        (interactive)
+        (require 'poly-R)
+        (require 'poly-markdown)
+        (R-mode)
+        (poly-markdown+r-mode))
+      ))
+  (use-package polymode
+    :mode (("\\.Rnw" . Rnw-mode))
+    :init
+    (progn
+      (defun Rnw-mode ()
+        "ESS LaTeX mode for Rnw files"
+        (interactive)
+        (require 'poly-R)
+        (require 'poly-noweb)
+        (R-mode)
+        (poly-noweb+r-mode))
+      ))
+  (use-package quarto-mode
+    :mode (("\\.qmd" . poly-quarto-mode))
+    )
+  )
+
+;;; packages.el ends here
